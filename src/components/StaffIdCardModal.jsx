2215f991f6561f9be30a9ca95d6a78239c01e259
Imoh Jimmy <hlccservice@gmail.com>
2026-09-11 17:20:20 +0100

    edited the url

--- src/components/StaffIdCardModal.jsx
+++ src/components/StaffIdCardModal.jsx
@@ -11,7 +11,7 @@ import {
 
 import axios from 'axios'
 
-const API_URL = import.meta.env.VITE_API_URL
+const API_URL = import.meta.env.VITE_BACKEND_URL
 
 const StaffIDCardModal = ({ visible, onClose, staff }) => {
   const [loading, setLoading] = useState(false)
